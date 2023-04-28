87187038ab3fcb29ccacab66a6b1a8315eb42891
Aureliano <aureliano.franca@hotmail.com>
2023-04-28 19:52:31 -0300

    Circuit breaker implementation.

--- circuit_breaker/circuit_breaker.go
+++ circuit_breaker/circuit_breaker.go
@@ -9,7 +9,9 @@ import (
 )
 
 var (
-	ErrCircuitBreakError = errors.New("...")
+	ErrThresholdError    = errors.New("threshold must be >= 1")
+	ErrResetTimeoutError = errors.New("reset timeout must be >= 1")
+	ErrCircuitIsOpen     = errors.New("circuit is open")
 )
 
 type Policy struct {
@@ -18,16 +20,117 @@ type Policy struct {
 	Errors               []error
 	BeforeCircuitBreaker func(p Policy)
 	AfterCircuitBreaker  func(p Policy, err error)
-	OnOpenCircuit        func(p Policy, err error)
-	OnHalfOpenCircuit    func(p Policy)
-	OnClosedCircuit      func(p Policy)
+	OnOpenCircuit        func(p Policy, status CircuitBreaker, err error)
+	OnHalfOpenCircuit    func(p Policy, status CircuitBreaker)
+	OnClosedCircuit      func(p Policy, status CircuitBreaker)
 }
+
+type CircuitState int
+
+type CircuitBreaker struct {
+	State            CircuitState
+	TimeErrorOcurred time.Time
+	ErrorCount       int
+}
+
+const (
+	ClosedState   = 0
+	OpenState     = 1
+	HalfOpenState = 2
+)
+
+var cbState = CircuitBreaker{}
 
 func New() Policy {
 	return Policy{
 		ThresholdErrors: 1,
-		ResetTimeout:    0,
+		ResetTimeout:    time.Second * 1,
 	}
 }
 
-func (p Policy) Run(ctx context.Context, cmd core.Command) error { return nil }
+func (p Policy) Run(ctx context.Context, cmd core.Command) error {
+	if err := validatePolicy(p); err != nil {
+		return err
+	}
+
+	if p.BeforeCircuitBreaker != nil {
+		p.BeforeCircuitBreaker(p)
+	}
+
+	setInitialState(p)
+
+	if cbState.State == OpenState {
+		return ErrCircuitIsOpen
+	}
+
+	err := cmd(ctx)
+
+	setPostState(p, err)
+
+	if p.AfterCircuitBreaker != nil {
+		p.AfterCircuitBreaker(p, err)
+	}
+
+	return nil
+}
+
+func setInitialState(p Policy) {
+	circuitIsOpen := cbState.State == OpenState
+	shouldChangeToHalfOpen := time.Since(cbState.TimeErrorOcurred) >= p.ResetTimeout
+
+	if circuitIsOpen && shouldChangeToHalfOpen {
+		cbState.State = HalfOpenState
+	}
+}
+
+func setPostState(p Policy, err error) {
+	if err != nil {
+		openCircuit(p, err)
+	} else {
+		switch cbState.State {
+		case HalfOpenState:
+			closeCircuit(p)
+		case OpenState:
+			halfOpenCircuit(p)
+		}
+	}
+}
+
+func openCircuit(p Policy, err error) {
+	cbState.State = OpenState
+	cbState.TimeErrorOcurred = time.Now()
+	cbState.ErrorCount++
+
+	if p.OnOpenCircuit != nil {
+		p.OnOpenCircuit(p, cbState, err)
+	}
+}
+
+func closeCircuit(p Policy) {
+	cbState.State = ClosedState
+	cbState.ErrorCount = 0
+
+	if p.OnClosedCircuit != nil {
+		p.OnClosedCircuit(p, cbState)
+	}
+}
+
+func halfOpenCircuit(p Policy) {
+	cbState.State = HalfOpenState
+
+	if p.OnHalfOpenCircuit != nil {
+		p.OnHalfOpenCircuit(p, cbState)
+	}
+}
+
+func validatePolicy(p Policy) error {
+	const minResetTimeout = time.Millisecond * 5
+	switch {
+	case p.ThresholdErrors < 1:
+		return ErrThresholdError
+	case p.ResetTimeout < minResetTimeout:
+		return ErrResetTimeoutError
+	default:
+		return nil
+	}
+}

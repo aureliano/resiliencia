8b30ef32975afe43c0eff150bc4663f03b076944
Aureliano <aureliano.franca@hotmail.com>
2023-04-29 18:19:29 -0300

    8 circuit breaker policy (#10)
    * Circuit breaker pattern archetype.
    
    * Circuit breaker implementation.
    
    * Remove attribute Errors from timeout policy.
    
    * Test cases scenarios.
    
    * Rename package circuit_breaker to circuitbreaker.
    
    * Rename package circuit_breaker to circuitbreaker.

---
+++ circuitbreaker/circuitbreaker.go
@@ -0,0 +1,149 @@
+package circuitbreaker
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/aureliano/resiliencia/core"
+)
+
+var (
+	ErrThresholdError    = errors.New("threshold must be >= 1")
+	ErrResetTimeoutError = errors.New("reset timeout must be >= 1")
+	ErrCircuitIsOpen     = errors.New("circuit is open")
+)
+
+type Policy struct {
+	ThresholdErrors      int
+	ResetTimeout         time.Duration
+	Errors               []error
+	BeforeCircuitBreaker func(p Policy, status CircuitBreaker)
+	AfterCircuitBreaker  func(p Policy, status CircuitBreaker, err error)
+	OnOpenCircuit        func(p Policy, status CircuitBreaker, err error)
+	OnHalfOpenCircuit    func(p Policy, status CircuitBreaker)
+	OnClosedCircuit      func(p Policy, status CircuitBreaker)
+}
+
+type CircuitState int
+
+type CircuitBreaker struct {
+	State            CircuitState
+	TimeErrorOcurred time.Time
+	ErrorCount       int
+}
+
+const (
+	ClosedState   = 0
+	OpenState     = 1
+	HalfOpenState = 2
+)
+
+var cbState = CircuitBreaker{}
+
+func Reset() {
+	cbState = CircuitBreaker{}
+}
+
+func New() Policy {
+	return Policy{
+		ThresholdErrors: 1,
+		ResetTimeout:    time.Second * 1,
+	}
+}
+
+func (p Policy) Run(ctx context.Context, cmd core.Command) error {
+	if err := validatePolicy(p); err != nil {
+		return err
+	}
+
+	if p.BeforeCircuitBreaker != nil {
+		p.BeforeCircuitBreaker(p, cbState)
+	}
+
+	setInitialState(p)
+
+	if cbState.State == OpenState {
+		return ErrCircuitIsOpen
+	}
+
+	err := cmd(ctx)
+
+	setPostState(p, err)
+
+	if p.AfterCircuitBreaker != nil {
+		p.AfterCircuitBreaker(p, cbState, err)
+	}
+
+	return nil
+}
+
+func setInitialState(p Policy) {
+	circuitIsOpen := cbState.State == OpenState
+	shouldChangeToHalfOpen := time.Since(cbState.TimeErrorOcurred) >= p.ResetTimeout
+
+	if circuitIsOpen && shouldChangeToHalfOpen {
+		halfOpenCircuit(p)
+	}
+}
+
+func setPostState(p Policy, err error) {
+	if err != nil && !handledError(p, err) {
+		openCircuit(p, err)
+	} else if cbState.State == HalfOpenState {
+		closeCircuit(p)
+	}
+}
+
+func openCircuit(p Policy, err error) {
+	cbState.State = OpenState
+	cbState.TimeErrorOcurred = time.Now()
+	cbState.ErrorCount++
+
+	if p.OnOpenCircuit != nil {
+		p.OnOpenCircuit(p, cbState, err)
+	}
+}
+
+func closeCircuit(p Policy) {
+	cbState.State = ClosedState
+	cbState.ErrorCount = 0
+
+	if p.OnClosedCircuit != nil {
+		p.OnClosedCircuit(p, cbState)
+	}
+}
+
+func halfOpenCircuit(p Policy) {
+	cbState.State = HalfOpenState
+
+	if p.OnHalfOpenCircuit != nil {
+		p.OnHalfOpenCircuit(p, cbState)
+	}
+}
+
+func handledError(p Policy, err error) bool {
+	if p.Errors == nil || len(p.Errors) == 0 {
+		return false
+	}
+
+	for _, expectedError := range p.Errors {
+		if errors.Is(expectedError, err) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func validatePolicy(p Policy) error {
+	const minResetTimeout = time.Millisecond * 5
+	switch {
+	case p.ThresholdErrors < 1:
+		return ErrThresholdError
+	case p.ResetTimeout < minResetTimeout:
+		return ErrResetTimeoutError
+	default:
+		return nil
+	}
+}
---
+++ circuitbreaker/circuitbreaker_test.go
@@ -0,0 +1,130 @@
+package circuitbreaker_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/aureliano/resiliencia/circuitbreaker"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	p := circuitbreaker.New()
+	assert.Equal(t, 1, p.ThresholdErrors)
+	assert.Equal(t, time.Second*1, p.ResetTimeout)
+}
+
+func TestRunValidatePolicyThresholdErrors(t *testing.T) {
+	p := circuitbreaker.Policy{ThresholdErrors: 0, ResetTimeout: time.Second * 1}
+	err := p.Run(context.TODO(), func(ctx context.Context) error { return nil })
+
+	assert.ErrorIs(t, err, circuitbreaker.ErrThresholdError)
+}
+
+func TestRunValidatePolicyResetTimeout(t *testing.T) {
+	p := circuitbreaker.Policy{ThresholdErrors: 1, ResetTimeout: time.Millisecond * 1}
+	err := p.Run(context.TODO(), func(ctx context.Context) error { return nil })
+
+	assert.ErrorIs(t, err, circuitbreaker.ErrResetTimeoutError)
+}
+
+func TestRunCircuitIsOpen(t *testing.T) {
+	circuitbreaker.Reset()
+	p := circuitbreaker.Policy{
+		ThresholdErrors:      1,
+		ResetTimeout:         time.Second * 1,
+		BeforeCircuitBreaker: func(p circuitbreaker.Policy, status circuitbreaker.CircuitBreaker) {},
+		OnOpenCircuit:        func(p circuitbreaker.Policy, status circuitbreaker.CircuitBreaker, err error) {},
+		OnHalfOpenCircuit:    func(p circuitbreaker.Policy, status circuitbreaker.CircuitBreaker) {},
+		OnClosedCircuit:      func(p circuitbreaker.Policy, status circuitbreaker.CircuitBreaker) {},
+		AfterCircuitBreaker:  func(p circuitbreaker.Policy, status circuitbreaker.CircuitBreaker, err error) {},
+	}
+
+	err := p.Run(context.TODO(), func(ctx context.Context) error { return fmt.Errorf("any error") })
+	assert.Nil(t, err)
+
+	err = p.Run(context.TODO(), func(ctx context.Context) error { return nil })
+	assert.ErrorIs(t, err, circuitbreaker.ErrCircuitIsOpen)
+}
+
+func TestRunCircuitHalfOpenSetToClosed(t *testing.T) {
+	circuitbreaker.Reset()
+	var state circuitbreaker.CircuitState
+	p := circuitbreaker.Policy{
+		ThresholdErrors:      1,
+		ResetTimeout:         time.Millisecond * 300,
+		BeforeCircuitBreaker: func(p circuitbreaker.Policy, status circuitbreaker.CircuitBreaker) {},
+		OnOpenCircuit:        func(p circuitbreaker.Policy, status circuitbreaker.CircuitBreaker, err error) {},
+		OnHalfOpenCircuit:    func(p circuitbreaker.Policy, status circuitbreaker.CircuitBreaker) {},
+		OnClosedCircuit:      func(p circuitbreaker.Policy, status circuitbreaker.CircuitBreaker) {},
+		AfterCircuitBreaker: func(p circuitbreaker.Policy, status circuitbreaker.CircuitBreaker, err error) {
+			state = status.State
+		},
+	}
+
+	err := p.Run(context.TODO(), func(ctx context.Context) error { return fmt.Errorf("any error") })
+	assert.Nil(t, err)
+	assert.EqualValues(t, circuitbreaker.OpenState, state)
+
+	err = p.Run(context.TODO(), func(ctx context.Context) error { return nil })
+	assert.ErrorIs(t, err, circuitbreaker.ErrCircuitIsOpen)
+	assert.EqualValues(t, circuitbreaker.OpenState, state)
+
+	time.Sleep(time.Millisecond * 300)
+	err = p.Run(context.TODO(), func(ctx context.Context) error { return nil })
+	assert.Nil(t, err)
+	assert.EqualValues(t, circuitbreaker.ClosedState, state)
+}
+
+func TestRunHandledErrors(t *testing.T) {
+	circuitbreaker.Reset()
+	var state circuitbreaker.CircuitState
+
+	errTest1 := errors.New("error test 1")
+	errTest2 := errors.New("error test 2")
+
+	p := circuitbreaker.Policy{
+		ThresholdErrors: 1,
+		ResetTimeout:    time.Millisecond * 300,
+		Errors:          []error{errTest1, errTest2},
+		AfterCircuitBreaker: func(p circuitbreaker.Policy, status circuitbreaker.CircuitBreaker, err error) {
+			state = status.State
+		},
+	}
+
+	err := p.Run(context.TODO(), func(ctx context.Context) error { return errTest1 })
+	assert.Nil(t, err)
+	assert.EqualValues(t, circuitbreaker.ClosedState, state)
+
+	err = p.Run(context.TODO(), func(ctx context.Context) error { return errTest2 })
+	assert.Nil(t, err)
+	assert.EqualValues(t, circuitbreaker.ClosedState, state)
+}
+
+func TestRunUnhandledError(t *testing.T) {
+	circuitbreaker.Reset()
+	var state circuitbreaker.CircuitState
+
+	errTest1 := errors.New("error test 1")
+	errTest2 := errors.New("error test 2")
+
+	p := circuitbreaker.Policy{
+		ThresholdErrors: 1,
+		ResetTimeout:    time.Millisecond * 300,
+		Errors:          []error{errTest1},
+		AfterCircuitBreaker: func(p circuitbreaker.Policy, status circuitbreaker.CircuitBreaker, err error) {
+			state = status.State
+		},
+	}
+
+	err := p.Run(context.TODO(), func(ctx context.Context) error { return errTest1 })
+	assert.Nil(t, err)
+	assert.EqualValues(t, circuitbreaker.ClosedState, state)
+
+	err = p.Run(context.TODO(), func(ctx context.Context) error { return errTest2 })
+	assert.Nil(t, err)
+	assert.EqualValues(t, circuitbreaker.OpenState, state)
+}

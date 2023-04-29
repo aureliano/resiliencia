a7139989f7e7ed5615799f90a95811e5b29d78c8
Aureliano <aureliano.franca@hotmail.com>
2023-04-29 19:39:24 -0300

    Exports function ErrorInErrors.

--- core/core.go
+++ core/core.go
@@ -1,9 +1,26 @@
 package core
 
-import "context"
+import (
+	"context"
+	"errors"
+)
 
 type Command func(ctx context.Context) error
 
 type Suplier interface {
 	Run(ctx context.Context, cmd Command) error
+}
+
+func ErrorInErrors(expectedErrors []error, err error) bool {
+	if len(expectedErrors) == 0 {
+		return false
+	}
+
+	for _, expectedError := range expectedErrors {
+		if errors.Is(expectedError, err) {
+			return true
+		}
+	}
+
+	return false
 }
